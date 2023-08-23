ca3a3738a22914fb7f7d12cda6fb6e15a350e8c1
srezasm <srezas.2017@gmail.com>
2023-08-23 16:27:30 +0330

    update validation model

--- notebooks/validation_model.py
+++ notebooks/validation_model.py
@@ -1,44 +1,45 @@
 import torch
 import torch.nn as nn
+import torch.functional as F
 
 
-class FaceValidateV1(nn.Module):
+class ValidationModel(nn.Module):
     def __init__(self, device) -> None:
         super().__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=2)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3)
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=16, kernel_size=4)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 16, 2, padding=1),
+            nn.Tanh(),
+            nn.BatchNorm2d(16)
+        )
 
-        self.dropout1 = nn.Dropout2d(p=0.4)
-        self.dropout2 = nn.Dropout2d(p=0.2)
-        self.dropout3 = nn.Dropout2d(p=0.1)
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, 3),
+            nn.Tanh(),
+            nn.BatchNorm2d(32)
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(32, 64, 4),
+            nn.Tanh(),
+            nn.BatchNorm2d(64)
+        )
 
         self.flatten = nn.Flatten()
 
-        self.tanh1 = nn.Linear(in_features=8*8, out_features=64)
-        self.tanh2 = nn.Linear(in_features=64, out_features=16)
-        self.tanh3 = nn.Linear(in_features=16, out_features=1)
-
-        self.sigmoid = nn.Sigmoid()
+        self.sigmoid = nn.Sequential(
+            nn.Linear(in_features=1024, out_features=1),
+            nn.Sigmoid()
+        )
 
         self.to(device)
 
     def forward(self, x):
-        x = torch.relu(self.conv1(x))
-        x = self.dropout1(x)
-
-        x = torch.relu(self.conv2(x))
-        x = self.dropout2(x)
-
-        x = torch.relu(self.conv3(x))
-        x = self.dropout3(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
 
         x = self.flatten(x)
-
-        x = torch.tanh(self.tanh1(x))
-        x = torch.tanh(self.tanh2(x))
-        x = torch.tanh(self.tanh3(x))
 
         x = self.sigmoid(x)
 
