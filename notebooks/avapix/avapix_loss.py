a63594df4be1be4b7f8cd28b91910f1fda6fea3b
srezasm <srezas.2017@gmail.com>
2023-08-23 16:28:22 +0330

    update avapix loss

--- notebooks/avapix/avapix_loss.py
+++ notebooks/avapix/avapix_loss.py
@@ -1,29 +1,20 @@
 import torch
 import torch.nn as nn
-import numpy as np
 
-# class AvapixLoss(nn.Module):
-#     def __init__(self, device) -> None:
-#         super().__init__()
 
-#         self.one_tensor = torch.tensor([1],
-#                                        device=device,
-#                                        requires_grad=True)
-#         self.device = device
-
-#     def forward(self, x):
-#         loss = torch.tan(self.one_tensor) / x - torch.tan(x)
-#         return torch.tensor(loss, device=self.device)
-    
 class AvapixLoss(nn.Module):
-    def __init__(self, device) -> None:
+    def __init__(self, zero_limit_value: float = 0.01) -> None:
         super().__init__()
 
-        self.one_tensor = torch.tensor([1.0],
-                                       device=device)
-        self.device = device
+        self.zero_limit_value = zero_limit_value
 
-    def forward(self, x):
-        loss = torch.tan(self.one_tensor) / x - torch.tan(x)
+    def forward(self, x: torch.Tensor):
+        device = x.device
+
+        x = x.masked_fill(x < self.zero_limit_value, self.zero_limit_value)
+
+        one_tensor = torch.tensor([1.0], device=device)
+        loss = torch.tan(one_tensor) / x - torch.tan(x)
         loss = loss.mean()
-        return loss.to(self.device).requires_grad_(True)
+
+        return loss.requires_grad_(True)
