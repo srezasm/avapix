3e54218faa96d36cfa753fbbf4d3f02c79d4c9db
srezasm <srezas.2017@gmail.com>
2023-08-24 14:32:25 +0330

    added difference pnalty loss to avapix model
    still the validation model needs improvement

--- notebooks/avapix/avapix_loss.py
+++ notebooks/avapix/avapix_loss.py
@@ -8,13 +8,29 @@ class AvapixLoss(nn.Module):
 
         self.zero_limit_value = zero_limit_value
 
-    def forward(self, x: torch.Tensor):
-        device = x.device
-
+    def __calc_face_shape_loss__(self, x: torch.Tensor):
         x = x.masked_fill(x < self.zero_limit_value, self.zero_limit_value)
 
-        one_tensor = torch.tensor([1.0], device=device)
+        one_tensor = torch.tensor([1.0], device=self.device)
         loss = torch.tan(one_tensor) / x - torch.tan(x)
         loss = loss.pow(2).mean()
+
+        return loss
+
+    def __calc_diff_loss__(self, output_pic: torch.Tensor, org_pic: torch.Tensor):
+        mask = org_pic != 0
+
+        result = torch.zeros_like(output_pic)
+        result[mask] = output_pic[mask]
+
+        loss = (result - org_pic).pow(2).mean()
+
+        return loss
+
+    def forward(self, x: torch.Tensor, output_pic: torch.Tensor, org_pic: torch.Tensor):
+        self.device = x.device
+
+        loss = self.__calc_face_shape_loss__(x)
+        loss += self.__calc_diff_loss__(output_pic, org_pic)
 
         return loss.requires_grad_(True)
