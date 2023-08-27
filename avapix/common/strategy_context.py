857bff42fcab7cf0882cf33d59ed8cf05d283289
srezasm <srezas.2017@gmail.com>
2023-08-27 22:17:03 +0330

    refactoring

--- avapix/common/strategy_context.py
+++ avapix/common/strategy_context.py
@@ -6,7 +6,9 @@ class StrategyContext:
         self.strategy = strategy
 
     def embed(self, text):
+        # TODO: add version parameter
         return self.strategy.embed(text)
 
     def extract(self, image_file):
+        # TODO: extract version from image_file
         return self.strategy.extract(image_file)
