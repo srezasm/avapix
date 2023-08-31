dbe27949411435bf8def9d844bec1f4c8b3cab6b
srezasm <srezas.2017@gmail.com>
2023-08-31 21:41:26 +0330

    change validation model

--- notebooks/validation_model.py
+++ notebooks/validation_model.py
@@ -1,57 +1,77 @@
 import torch
 import torch.nn as nn
-import torch.functional as F
+import torch.nn.functional as F
+import numpy as np
 
 
 class ValidationModel(nn.Module):
     def __init__(self) -> None:
         super().__init__()
 
-        self.conv1_encode = nn.Sequential(
-            nn.Conv2d(3, 6, 1),
-            nn.LeakyReLU(),
+        self.conv1 = nn.Conv2d(3, 8, 3, 1)
+        self.conv2 = nn.Conv2d(8, 16, 3, 1)
+        self.conv3 = nn.Conv2d(16, 32, 2, 1)
+        self.conv4 = nn.Conv2d(32, 64, 2, 1)
 
-            nn.Conv2d(6, 8, 2),
-            nn.LeakyReLU(),
-
-            nn.Conv2d(8, 16, 4),
-            nn.LeakyReLU()
-        )
-
-        self.conv2_decode = nn.Sequential(
-            nn.ConvTranspose2d(16, 8, 4),
-            nn.ReLU(),
-
-            nn.ConvTranspose2d(8, 6, 2),
-            nn.ReLU(),
-
-            nn.ConvTranspose2d(6, 3, 1),
-            nn.ReLU()
-        )
-
-        self.conv3_sym = nn.Sequential(
-            nn.Conv2d(3, 32, (8, 4)),
-            nn.ReLU(),
-            nn.Tanh(),
-        )
+        self.batchnorm1 = nn.BatchNorm2d(8)
+        self.batchnorm2 = nn.BatchNorm2d(16)
+        self.batchnorm3 = nn.BatchNorm2d(32)
 
         self.flatten = nn.Flatten()
 
-        self.sigmoid = nn.Sequential(
-            nn.Linear(in_features=160, out_features=1),
-            nn.Sigmoid()
-        )
+        self.linear1 = nn.Linear(256, 128)
+        self.linear2 = nn.Linear(128, 64)
+        self.linear3 = nn.Linear(64, 32)
+        self.linear4 = nn.Linear(32, 16)
+        self.linear5 = nn.Linear(16, 1)
+
+        self.batchnorm4 = nn.BatchNorm1d(64)
+        self.batchnorm5 = nn.BatchNorm1d(16)
 
     def forward(self, x):
-        device = x.device
-        self.to(device)
+        x = F.relu(self.conv1(x))
+        x = self.batchnorm1(x)
 
-        x = self.conv1_encode(x)
-        x = self.conv2_decode(x)
-        x = self.conv3_sym(x)
+        x = F.relu(self.conv2(x))
+        x = self.batchnorm2(x)
+
+        x = F.relu(self.conv3(x))
+        x = self.batchnorm3(x)
+
+        x = F.relu(self.conv4(x))
 
         x = self.flatten(x)
 
-        x = self.sigmoid(x)
+        x = F.tanh(self.linear1(x))
+        
+        x = F.leaky_relu(self.linear2(x))
+        x = self.batchnorm4(x)
+
+        x = F.tanh(self.linear3(x))
+
+        x = F.leaky_relu(self.linear4(x))
+        x = self.batchnorm5(x)
+
+        x = F.sigmoid(self.linear5(x))
 
         return x
+
+
+def findConv2dOutShape(hin, win, conv, pool=2):
+    # get conv arguments
+    kernel_size = conv.kernel_size
+    stride = conv.stride
+    padding = conv.padding
+    dilation = conv.dilation
+
+    hout = np.floor(
+        (hin + 2 * padding[0] - dilation[0] * (kernel_size[0] - 1) - 1) / stride[0] + 1
+    )
+    wout = np.floor(
+        (win + 2 * padding[1] - dilation[1] * (kernel_size[1] - 1) - 1) / stride[1] + 1
+    )
+
+    if pool:
+        hout /= pool
+        wout /= pool
+    return int(hout), int(wout)
