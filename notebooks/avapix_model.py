8a35d6bd0ceda9b6140091b7b6d86ecdba9bfaed
srezasm <srezas.2017@gmail.com>
2023-09-06 19:12:48 +0330

    update avapix model

--- notebooks/avapix_model.py
+++ notebooks/avapix_model.py
@@ -7,63 +7,104 @@ class AvapixModel(nn.Module):
     def __init__(self) -> None:
         super().__init__()
 
-        self.encode1 = nn.Sequential(
-            nn.Conv2d(3, 8, 2),  # 7x7
-            nn.ReLU(),
-            nn.Conv2d(8, 16, 3),  # 4x4
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),  # 2x2
-            nn.Tanh(),
-            nn.BatchNorm2d(16),
-        )
+        # CNN
+        self.conv1 = nn.Conv2d(3, 16, 2)
+        self.do1 = nn.Dropout(0.5)
 
-        self.encode2 = nn.Sequential(
-            nn.Conv2d(3, 16, 2, 2),  # 4x4
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),  # 2x2
-            nn.Tanh(),
-            nn.BatchNorm2d(16),
-        )
+        self.conv2 = nn.Conv2d(16, 32, 2)
+        self.do2 = nn.Dropout(0.5)
 
-        self.encode3 = nn.Sequential(
-            nn.Conv2d(3, 16, 4, 4),
-            nn.Tanh(),
-            nn.BatchNorm2d(16)  # 2x2
-        )
+        self.conv3 = nn.Conv2d(32, 64, 2)
+        self.do3 = nn.Dropout(0.5)
 
-        self.conv = nn.Sequential(
-            nn.Conv2d(48, 64, 1),
-            nn.ReLU(),
+        self.conv4 = nn.Conv2d(64, 128, 2)
+        self.do4 = nn.Dropout(0.5)
 
-            nn.Conv2d(64, 128, 1),
-            nn.Tanh(),
+        self.conv5 = nn.Conv2d(128, 256, 2)
+        self.do5 = nn.Dropout(0.5)
 
-            nn.Conv2d(128, 256, 1),
-            nn.ReLU(),
-            nn.BatchNorm2d(256)
-        )
+        self.conv6 = nn.Conv2d(256, 512, 2)
+        self.bn = nn.BatchNorm2d(512)
+        self.do6 = nn.Dropout(0.5)
 
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(256, 64, 1),
-            nn.ReLU(),
-            nn.ConvTranspose2d(64, 16, 2),
-            nn.Tanh(),
-            nn.ConvTranspose2d(16, 8, 3),
-            nn.ReLU(),
-            nn.ConvTranspose2d(8, 3, 4),
-            nn.Tanh(),
-            nn.BatchNorm2d(3)
-        )
+        self.conv7 = nn.ConvTranspose2d(512, 256, 2)
+        self.do7 = nn.Dropout(0.5)
+
+        self.conv8 = nn.ConvTranspose2d(256, 128, 2)
+        self.do8 = nn.Dropout(0.5)
+
+        self.conv9 = nn.ConvTranspose2d(128, 64, 2)
+        self.do9 = nn.Dropout(0.5)
+
+        self.conv10 = nn.ConvTranspose2d(64, 32, 2)
+        self.do10 = nn.Dropout(0.5)
+
+        self.conv11 = nn.ConvTranspose2d(32, 16, 2)
+        self.do11 = nn.Dropout(0.5)
+
+        self.conv12 = nn.ConvTranspose2d(16, 3, 2)
+
+        # Weight initialization
+        nn.init.kaiming_normal_(self.conv1.weight, nonlinearity="relu")
+        nn.init.constant_(self.conv1.bias, 0)
+        nn.init.xavier_normal_(self.conv2.weight)
+        nn.init.constant_(self.conv2.bias, 0)
+        nn.init.xavier_normal_(self.conv3.weight)
+        nn.init.constant_(self.conv3.bias, 0)
+        nn.init.kaiming_normal_(self.conv4.weight, nonlinearity="relu")
+        nn.init.constant_(self.conv4.bias, 0)
+        nn.init.xavier_normal_(self.conv5.weight)
+        nn.init.constant_(self.conv5.bias, 0)
+        nn.init.xavier_normal_(self.conv6.weight)
+        nn.init.constant_(self.conv6.bias, 0)
+        nn.init.kaiming_normal_(self.conv7.weight, nonlinearity="relu")
+        nn.init.constant_(self.conv7.bias, 0)
+        nn.init.xavier_normal_(self.conv8.weight)
+        nn.init.constant_(self.conv8.bias, 0)
+        nn.init.xavier_normal_(self.conv9.weight)
+        nn.init.constant_(self.conv9.bias, 0)
+        nn.init.kaiming_normal_(self.conv10.weight, nonlinearity="relu")
+        nn.init.constant_(self.conv10.bias, 0)
+        nn.init.xavier_normal_(self.conv11.weight)
+        nn.init.constant_(self.conv11.bias, 0)
+        nn.init.xavier_normal_(self.conv12.weight)
+        nn.init.constant_(self.conv12.bias, 0)
 
     def forward(self, img):
-        img1 = self.encode1(img)
-        img2 = self.encode2(img)
-        img3 = self.encode3(img)
+        img = torch.relu(self.conv1(img))
+        img = self.do1(img)
 
-        img = torch.concat((img1, img2, img3), dim=1)
+        img = torch.tanh(self.conv2(img))
+        img = self.do2(img)
 
-        img = self.conv(img)
+        img = torch.sigmoid(self.conv3(img))
+        img = self.do3(img)
 
-        img = self.decoder(img)
+        img = torch.relu(self.conv4(img))
+        img = self.do4(img)
+
+        img = torch.tanh(self.conv5(img))
+        img = self.do5(img)
+
+        img = torch.sigmoid(self.conv6(img))
+        img = self.bn(img)
+        img = self.do6(img)
+
+        img = torch.relu(self.conv7(img))
+        img = self.do7(img)
+
+        img = torch.tanh(self.conv8(img))
+        img = self.do8(img)
+
+        img = torch.sigmoid(self.conv9(img))
+        img = self.do9(img)
+
+        img = torch.relu(self.conv10(img))
+        img = self.do10(img)
+
+        img = torch.tanh(self.conv11(img))
+        img = self.do11(img)
+
+        img = torch.sigmoid(self.conv12(img))
 
         return img
