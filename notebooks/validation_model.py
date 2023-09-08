97ff9cbfcd47602d1c4c1b27661afa6dece34666
srezasm <srezas.2017@gmail.com>
2023-09-08 18:57:10 +0330

    update validation model

--- notebooks/validation_model.py
+++ notebooks/validation_model.py
@@ -16,41 +16,32 @@ class ValidationModel(nn.Module):
 
         self.conv3 = nn.Conv2d(3, 16, 4)
         self.mp3 = nn.MaxPool2d(2)
+        self.conv4 = nn.Conv2d(16, 32, 2)
 
 
         # Linear
-        self.bn = nn.BatchNorm1d(128)
+        self.bn = nn.BatchNorm1d(96)
 
-        self.ln1 = nn.Linear(128, 256)
+        self.ln1 = nn.Linear(96, 256)
         self.do1 = nn.Dropout(0.5)
-        
-        self.ln2 = nn.Linear(256, 128)
-        self.do2 = nn.Dropout(0.5)
 
-        self.ln3 = nn.Linear(128, 64)
+        self.ln2 = nn.Linear(256, 512)
+        self.do2 = nn.Dropout(0.5)
+        
+        self.ln3 = nn.Linear(512, 256)
         self.do3 = nn.Dropout(0.5)
 
-        self.ln4 = nn.Linear(64, 1)
-        
-   
-        # Weight initialization
-        #     CNN
-        nn.init.kaiming_normal_(self.conv1.weight, nonlinearity="relu")
-        nn.init.constant_(self.conv1.bias, 0)
-        nn.init.kaiming_normal_(self.conv2.weight, nonlinearity="relu")
-        nn.init.constant_(self.conv2.bias, 0)
-        nn.init.kaiming_normal_(self.conv3.weight, nonlinearity="relu")
-        nn.init.constant_(self.conv3.bias, 0)
+        self.ln4 = nn.Linear(256, 128)
+        self.do4 = nn.Dropout(0.5)
 
-        #    Linear
-        nn.init.xavier_normal_(self.ln1.weight)
-        nn.init.constant_(self.ln1.bias, 0)
-        nn.init.xavier_normal_(self.ln2.weight)
-        nn.init.constant_(self.ln2.bias, 0)
-        nn.init.xavier_normal_(self.ln3.weight)
-        nn.init.constant_(self.ln3.bias, 0)
-        nn.init.xavier_normal_(self.ln4.weight)
-        nn.init.constant_(self.ln4.bias, 0)
+        self.ln5 = nn.Linear(128, 64)
+        self.do5 = nn.Dropout(0.5)
+
+        self.ln6 = nn.Linear(64, 32)
+        self.do6 = nn.Dropout(0.5)
+
+        self.ln7 = nn.Linear(32, 1)
+
 
     def forward(self, img):
         # CNN
@@ -62,8 +53,9 @@ class ValidationModel(nn.Module):
         img2 = self.mp2(img2)
         img2 = img2.reshape(img2.shape[0], -1)
 
-        img3 = self.conv3(img)
+        img3 = torch.relu(self.conv3(img))
         img3 = self.mp3(img3)
+        img3 = torch.relu(self.conv4(img3))
         img3 = img3.reshape(img3.shape[0], -1)
 
         # Concatenate
@@ -76,12 +68,21 @@ class ValidationModel(nn.Module):
         img = torch.tanh(self.ln1(img))
         img = self.do1(img)
 
-        img = torch.sigmoid(self.ln2(img))
+        img = torch.relu(self.ln2(img))
         img = self.do2(img)
 
         img = torch.tanh(self.ln3(img))
         img = self.do3(img)
 
-        img = torch.sigmoid(self.ln4(img))
+        img = torch.relu(self.ln4(img))
+        img = self.do4(img)
+
+        img = torch.tanh(self.ln5(img))
+        img = self.do5(img)
+
+        img = torch.relu(self.ln6(img))
+        img = self.do6(img)
+
+        img = torch.sigmoid(self.ln7(img))
         
         return img
